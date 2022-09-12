11a8b07fc5a8d21c735886897c82e4393ea2cf69
Falk Marinov <mail@falkmarinov.de>
2022-09-12 23:18:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Falk's personal website
+
+Inspired by a business card.
+
+![website](./docs/assets/website.png)
---
+++ pages/index.tsx
@@ -0,0 +1,82 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+import { FaLinkedinIn } from 'react-icons/fa';
+import { GrMail } from 'react-icons/gr';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { TypeAnimation } from 'react-type-animation';
+
+const Home: NextPage = () => {
+  const links: (JSX.Element | string)[][] = [
+    [
+      <GrMail key={'mail-icon'} />,
+      'mail@falkmarinov.de',
+      'mailto:mail@falkmarinov.de',
+    ],
+    [
+      <FaLinkedinIn key={'linkedin-icon'} />,
+      '/falkmarinov',
+      'https://www.linkedin.com/in/falkmarinov',
+    ],
+    [<BsFillPersonFill key={'person-icon'} />, 'personal resume', ''],
+  ];
+
+  const sequence = [
+    '',
+    1000,
+    'focussed.',
+    1000,
+    'focussed. adaptable.',
+    1000,
+    'focussed. adaptable. logical.',
+    1000,
+    'focussed. adaptable. logical. keen.',
+    2000,
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>Falk Marinov</title>
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='description' content='Personal website of Falk Marinov' />
+      </Head>
+
+      <main className='h-screen font-mono sm:text-xl lg:text-2xl'>
+        <div className='h-full lg:columns-2 lg:gap-0'>
+          {/* left section */}
+          <div className='grid bg-gray-200 bg-repeat place-content-center h-1/2 lg:h-full bg-circuit-board'>
+            <div>
+              <h1 className='text-3xl sm:text-4xl lg:text-5xl tracking-[0.16em] font-bold'>
+                Falk Marinov
+              </h1>
+              <TypeAnimation repeat={Infinity} sequence={sequence} />
+            </div>
+          </div>
+          {/* right section */}
+          <div className='grid place-content-center h-1/2 lg:h-full bg-gray-50 '>
+            <div className='flex flex-col gap-5'>
+              {links.map(([icon, text, url]) => {
+                return (
+                  <a
+                    key={text as string}
+                    className='flex items-center gap-5 group'
+                    href={url as string}
+                  >
+                    <div className='grid w-12 h-12 transition duration-300 bg-gray-200 rounded-full place-content-center lg:text-lg group-hover:bg-gray-300'>
+                      {icon}
+                    </div>
+                    <span className='group-hover:underline'>{text}</span>
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
