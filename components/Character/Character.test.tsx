71506604c535f832c800f1429b4e764548275646
Falk Marinov <mail@falkmarinov.de>
2022-09-28 13:20:25 +0200

    test(Character): refactor element select

--- components/Character/Character.test.tsx
+++ components/Character/Character.test.tsx
@@ -9,14 +9,18 @@ describe('<Character />', () => {
   describe('prop: isHovered', () => {
     it('should be bg-gray-300', () => {
       render(<Character isHovered>I</Character>);
-      expect(screen.getByText('I')).toHaveClass('bg-gray-300');
-      expect(screen.getByText('I')).not.toHaveClass('bg-gray-200');
+      const child = screen.getByText('I');
+      const parent = child.parentElement as HTMLElement;
+      expect(parent).toHaveClass('bg-gray-300');
+      expect(parent).not.toHaveClass('bg-gray-200');
     });
 
     it('otherwise be bg-gray-200', () => {
       render(<Character>I</Character>);
-      expect(screen.getByText('I')).toHaveClass('bg-gray-200');
-      expect(screen.getByText('I')).not.toHaveClass('bg-gray-300');
+      const child = screen.getByText('I');
+      const parent = child.parentElement as HTMLElement;
+      expect(parent).toHaveClass('bg-gray-200');
+      expect(parent).not.toHaveClass('bg-gray-300');
     });
   });
 });
