00042a74b1b82feaae316f72e2dcc499378b6cef
Falk Marinov <mail@falkmarinov.de>
2022-10-11 15:08:38 +0200

    test(LinkList): add snapshot tests for LinkList

--- src/components/LinkList/LinkList.test.tsx
+++ src/components/LinkList/LinkList.test.tsx
@@ -1,22 +1,46 @@
-import { render, screen, within } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { FaAddressBook } from 'react-icons/fa';
-import { URL } from 'url';
 
 import { LinkList } from './LinkList';
 
 describe('<LinkList />', () => {
-  it('should render', () => {
-    render(
-      <LinkList
-        linkItems={[
-          {
-            icon: <FaAddressBook />,
-            label: 'Example',
-            url: new URL('https://example.com/'),
-          },
-        ]}
-      />
-    );
+  describe('should render different amounts of items', () => {
+    it('One item', () => {
+      const { container } = render(
+        <LinkList
+          linkItems={[
+            {
+              icon: <FaAddressBook />,
+              label: 'Example',
+              url: 'https://www.example.com/',
+            },
+          ]}
+        />
+      );
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('Two items', () => {
+      const { container } = render(
+        <LinkList
+          linkItems={[
+            {
+              icon: <FaAddressBook />,
+              label: 'Example 1',
+              url: 'https://www.example.com/',
+            },
+            {
+              icon: <FaAddressBook />,
+              label: 'Example 2',
+              url: 'https://www.example.com/',
+            },
+          ]}
+        />
+      );
+
+      expect(container).toMatchSnapshot();
+    });
   });
 });
