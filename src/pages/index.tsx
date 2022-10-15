8cee3bba648146cdac3304c7d761b432531b8d39
Falk Marinov <mail@falkmarinov.de>
2022-10-15 18:12:33 +0200

    fix(language select): sync right with top position

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -83,7 +83,7 @@ const Home: NextPage = () => {
         <select
           defaultValue={router.locale}
           onChange={(e) => router.push('', '', { locale: e.target.value })}
-          className='absolute bg-transparent font-bold lg:font-normal top-[3vh] right-[3vw]'
+          className='absolute bg-transparent font-bold lg:font-normal top-[3vh] right-[3vh]'
         >
           {router.locales?.sort().map((locale, index) => {
             return (
